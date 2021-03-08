995064f4d177248476e4c14d49e3dc51ef3222c1
明洁 <3284304056@qq.com>
2021-03-08 16:28:18 +0800

    fix: 测试husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-ReactDom.render(<div>wangmingjie</div>, document.getElementById("root"));
+ReactDom.render(<div>HELLO</div>, document.getElementById("root"));
