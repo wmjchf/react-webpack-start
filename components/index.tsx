65868f23527a26010d2fe157191746becfb9272f
明洁 <3284304056@qq.com>
2021-03-18 14:48:10 +0800

    feat: version1.1.7

--- components/index.tsx
+++ components/index.tsx
@@ -2,4 +2,4 @@
 export type { FormValues } from "./form";
 export { default as Form } from "./form";
 // InputItem组件
-export { default as InputItem } from "./inputItem";
+export { default as InputItem } from "./input-item";
